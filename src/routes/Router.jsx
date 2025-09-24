f020d309cc93e4a17c03bbbb72fd024aaa7d703f
🚀mcoria🚀 <coria.marcelo.90@gmail.com>
2025-09-23 22:34:45 -0300

    🚀feat: config init

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,13 +1,38 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import WorkerPage from '../pages/worker/WorkerPage.jsx'
-import AgendarCertificadoPage from '../pages/agendar-certificado/AgendarCertificadoPage.jsx'
+import ComponentesPage from '../pages/examplee-components/ComponentesPage.jsx'
+import Login from '../pages/auth/login/components/Login.jsx'
+import ProtectedRoute from '../components/auth/ProtectedRoute.jsx'
+import RedirectAuthenticatedUser from '../components/auth/RedirectAuthenticatedUser.jsx'
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path='/' element={<h1>Pagina de inicio</h1>} />
-      <Route path='/info-trabajador' element={<WorkerPage />} />
-      <Route path='/agendar-certificado' element={<AgendarCertificadoPage />} />
+      <Route
+        path="/"
+        element={
+          <RedirectAuthenticatedUser>
+            <Login />
+          </RedirectAuthenticatedUser>
+        }
+      />
+      <Route
+        path="/info-trabajador"
+        element={
+          <ProtectedRoute>
+            <WorkerPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/example-components"
+        element={
+          <ProtectedRoute>
+            <ComponentesPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
 }
